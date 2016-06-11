bd1bce3efcb400b739c04528841b66bd6ba6f963
David Galante <davidgalante77@gmail.com>
2016-06-12 00:13:14 +0200

    c'est vraiment de la merde java et les interfaces

--- JavaFXApplication3/src/javafxapplication3/JavaFXApplication3.java
+++ JavaFXApplication3/src/javafxapplication3/JavaFXApplication3.java
@@ -3,38 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package projectjava;
+package javafxapplication3;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
  *
  * @author david
  */
-public class ProjectJava extends Application {
+public class JavaFXApplication3 extends Application {
     
     @Override
-    public void start(Stage primaryStage) {     
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
         StackPane root = new StackPane();
-        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
-        double longueurScene=1280;
-        double hauteurScene=900;
-        Scene scene = new Scene(root,longueurScene,hauteurScene);
-        primaryStage.setTitle("Bibliothèque");
-        MenuPrincipal mn=new MenuPrincipal();
-        root.getChildren().add(mn);
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
-        scene.getStylesheets().add(ProjectJava.class.getResource("Bibliotheque.css").toExternalForm());
         primaryStage.show();
     }
 
--- ProjectJava/src/projectjava/MenuPrincipal.java
+++ ProjectJava/src/projectjava/MenuPrincipal.java
@@ -6,14 +6,19 @@
 package projectjava;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 /**
  *
@@ -22,10 +27,19 @@ import javafx.scene.paint.Color;
 public class MenuPrincipal extends GridPane{
     
     public MenuPrincipal(){
-        Button btnMl=new Button("Menu lecture");
+        super();
+        this.setMaxSize(400, 400);
+        Label titre=new Label("BIBLIOTHEQUE");
+        this.setId("menu-principal");
+        Button btnMl=new Button("Menu Lecture");
         Button btnMe=new Button("Menu Ecriture");
-        GridPane.setConstraints(btnMl, 0, 0);
-        GridPane.setConstraints(btnMe, 0, 1);
-        this.getChildren().addAll(btnMl,btnMe);
+        GridPane.setConstraints(titre, 0, 0);
+        GridPane.setConstraints(btnMl, 0, 1);
+        GridPane.setConstraints(btnMe, 0, 2);
+        btnMl.setPrefSize(400, 50);
+        btnMe.setPrefSize(400, 50);
+        this.setVgap(15);
+        this.getChildren().addAll(titre,btnMl,btnMe);
+        this.setAlignment(Pos.CENTER);
     }
 }
