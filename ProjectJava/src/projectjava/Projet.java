29e39d32fc668c6da4a62cffb31dcbd4953891a9
Ghiles <Ghiles@Riles-PC>
2016-06-14 20:10:57 +0200

    Divers.

--- ProjectJava/src/projectjava/Projet.java
+++ ProjectJava/src/projectjava/Projet.java
@@ -16,14 +16,14 @@ public class Projet {
         bibli_test.addLivre(new Livre("Walid Adventure3",hauteur,"HEN",1,bibli_test));
         bibli_test.addAdherent(adh_test);
         bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Abdellah","Ghiles","azeaze@gmail.com","Bananier",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Galente","David","azeaze@gmail.com","Plombier",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Dieu","Arnaud","azeaze@gmail.com","Coiffeur",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Boyer","Guyaume","azeaze@gmail.com","Sodomite",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Hedili","Abdelsalem","azeaze@gmail.com","Pas tres Charlie",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Moustawi","Walid","azeaze@gmail.com","Malade",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Kissi","Naim","azeaze@gmail.com","Mediateur",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Koulibay","Amedi","azeaze@gmail.com","Salafiste",new Adresse("29","rue des grateur","99999","Partout")));
-        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"fdsfsd","fsfsd","azeaze@gmail.com","ezez",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Galente","David","fsdf@gmail.com","Plombier",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Dieu","Arnaud","xcvxc@gmail.com","Coiffeur",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Boyer","Guyaume","fgdgfdgtr@gmail.com","Sodomite",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Hedili","Abdelsalem","mlkmlk@gmail.com","Pas tres Charlie",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Moustawi","Walid","hjgjg@gmail.com","Malade",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Kissi","Naim","zez@gmail.com","Mediateur",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"Koulibay","Amedi","rfds@gmail.com","Salafiste",new Adresse("29","rue des grateur","99999","Partout")));
+        bibli_test.addAdherent(new Adherent(bibli_test.getID(),"fdsfsd","fsfsd","tryr@gmail.com","ezez",new Adresse("29","rue des grateur","99999","Partout")));
         
         System.out.println(bibli_test.Livre_toString());
         System.out.println(adh_test.toString());
